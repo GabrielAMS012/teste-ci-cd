b1f47af8c70fed247e2df482906041479c551870
gabrielAMS012 <gabriel.marassi@gmail.com>
2025-09-26 21:05:57 -0300

    first commit

---
+++ src/main/java/com/example/teste_ci_cd/TesteCiCdApplication.java
@@ -0,0 +1,13 @@
+package com.example.teste_ci_cd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TesteCiCdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TesteCiCdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/teste_ci_cd/TesteCiCdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.teste_ci_cd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TesteCiCdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
